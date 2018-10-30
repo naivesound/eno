a5b299632e64dafb8632edade5f4e8b502c78971
Sergii Zaitsev <sergii.zaitsev@sixt.com>
2018-10-30 14:46:26 +0100

    add osc server

--- cmd/eno/main.go
+++ cmd/eno/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net"
 	"os"
 	"os/signal"
+	"reflect"
 	"time"
 
+	"github.com/hypebeast/go-osc/osc"
 	"github.com/naivesound/eno/pkg/looper"
 	"github.com/naivesound/eno/pkg/metronome"
 	"github.com/naivesound/eno/pkg/synth"
@@ -48,6 +53,26 @@ func startAudio(ctx context.Context, sampleRate int, cb func([]int16)) error {
 	return nil
 }
 
+func OSCHandler(f interface{}) osc.HandlerFunc {
+	return func(msg *osc.Message) {
+		call := reflect.ValueOf(f)
+		if len(msg.Arguments) != call.Type().NumIn() {
+			log.Println("bad number of OSC arguments:", len(msg.Arguments), "!=", call.Type().NumIn())
+			return
+		}
+		args := make([]reflect.Value, call.Type().NumIn())
+		for i := 0; i < len(args); i++ {
+			arg := reflect.ValueOf(msg.Arguments[i])
+			if !arg.Type().ConvertibleTo(call.Type().In(i)) {
+				log.Println("argument", i, "is not convertible")
+				return
+			}
+			args[i] = arg.Convert(call.Type().In(i))
+		}
+		call.Call(args)
+	}
+}
+
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -63,8 +88,8 @@ func main() {
 	synth.SetGain(0.4)
 
 	m := metronome.New(sampleRate)
-	m.SetBPM(120)
-	m.SetGain(0.1)
+	m.SetBPM(0)
+	m.SetGain(0.2)
 
 	looper := looper.New(sampleRate)
 
@@ -73,6 +98,32 @@ func main() {
 		looper.MixStereo(out)
 		m.MixStereo(out)
 	})
+
+	go func() {
+		srv := &osc.Server{}
+
+		srv.Handle("/eno/synth/on", OSCHandler(synth.NoteOn))
+		srv.Handle("/eno/synth/off", OSCHandler(synth.NoteOff))
+		srv.Handle("/eno/synth/pitch", OSCHandler(synth.PitchBend))
+		srv.Handle("/eno/synth/cc", OSCHandler(synth.ControlChange))
+		srv.Handle("/eno/synth/load", OSCHandler(synth.Load))
+		srv.Handle("/eno/synth/gain", OSCHandler(synth.SetGain))
+
+		srv.Handle("/eno/metronome/tap", OSCHandler(m.Tap))
+		srv.Handle("/eno/metronome/gain", OSCHandler(m.SetGain))
+		srv.Handle("/eno/metronome/bpm", OSCHandler(m.SetBPM))
+
+		for port := 7027; port < 7030; port++ {
+			ln, err := net.ListenPacket("udp", fmt.Sprintf("127.0.0.1:%d", port))
+			if err != nil {
+				continue
+			}
+			log.Println("starting OSC server on UDP port", port)
+			srv.Serve(ln)
+			return
+		}
+		log.Println("failed to find a port in the range 7027..7029")
+	}()
 
 	<-sigc
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/naivesound/eno
 
 require (
+	github.com/hypebeast/go-osc v0.0.0-20170205175741-592fa95c06c9
 	github.com/naivesound/tsf v0.1.0
 	github.com/thestk/rtaudio v0.0.0-20181030000000-6ee55842a5b0
 )
